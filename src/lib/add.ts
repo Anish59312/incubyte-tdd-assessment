7ef049acc08ea61db2843841c96a75206b9cd8e8
Anish59312 <anish31442@gmail.com>
2025-07-03 22:12:17 +0530

    [step-1 refactor] edited add function to inlcude number return type

--- src/lib/add.ts
+++ src/lib/add.ts
@@ -1,4 +1,4 @@
-export default function add(numbers: string) {
+export default function add(numbers: string): number {
     if(!numbers || numbers.trim() == '')
         return 0;
     return parseInt(numbers);
