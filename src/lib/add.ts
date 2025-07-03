bb1bae30409cdc6ac210dd09560d0463d86cdd0e
Anish59312 <anish31442@gmail.com>
2025-07-03 23:14:21 +0530

    [step-3 add] added code to pass the test

--- src/lib/add.ts
+++ src/lib/add.ts
@@ -1,13 +1,12 @@
 export default function add(numbers: string): number {
   if (!numbers || numbers.trim() == "") return 0;
 
-  let numberArray: number[] = numbers.split(",").map((x) => {
+  let numberArray: number[] = numbers.split(/[,\n]+/).map((x) => {
+    console.log("x", x);
     x = x.trim();
     if (x == "" || isNaN(parseInt(x, 10))) return 0;
     return parseInt(x, 10);
   });
-
-  console.log("numberArray", numberArray)
 
   return numberArray.reduce((sum, num) => sum+num, 0)
 }
