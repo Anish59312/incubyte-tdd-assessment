f2de77d2478b26f27e6932cd27270f43f9f3d6ee
Anish59312 <anish31442@gmail.com>
2025-07-03 23:21:00 +0530

    [step-3 refactor] imporved add function code quality

--- src/lib/add.ts
+++ src/lib/add.ts
@@ -1,12 +1,12 @@
 export default function add(numbers: string): number {
   if (!numbers || numbers.trim() == "") return 0;
 
-  let numberArray: number[] = numbers.split(/[,\n]+/).map((x) => {
-    console.log("x", x);
-    x = x.trim();
-    if (x == "" || isNaN(parseInt(x, 10))) return 0;
-    return parseInt(x, 10);
-  });
+  let numberArray: number[] = numbers
+  .split(/[,\n]+/)
+  .map(x => x.trim())
+  .filter(x => x !== "")
+  .map(x => parseInt(x, 10))
+  .filter(x => !isNaN(x))
 
   return numberArray.reduce((sum, num) => sum+num, 0)
 }
