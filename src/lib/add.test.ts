e6d289bac495ec0b2a9bf62bbc8787743b597aa8
Anish59312 <anish31442@gmail.com>
2025-07-03 22:09:21 +0530

    [step-1 test] added test to output the same number on input

--- src/lib/add.test.ts
+++ src/lib/add.test.ts
@@ -3,5 +3,9 @@ import add from "./add";
 describe("string calculator", () => {
     test("empty string should return ", () => {
       expect(add("")).toBe(0);
-    });      
+    });   
+    
+    test("single number 1 should output 1", () =>{
+        expect(add("1")).toBe(1)
+    })
 })
