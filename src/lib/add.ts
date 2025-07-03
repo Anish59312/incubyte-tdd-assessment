5878ec05b1723dd9e5e5e0089083febb051dcbe9
Anish59312 <anish31442@gmail.com>
2025-07-03 22:32:31 +0530

    [step-1 add] added code to pass the test

--- src/lib/add.ts
+++ src/lib/add.ts
@@ -1,5 +1,18 @@
 export default function add(numbers: string): number {
+
     if(!numbers || numbers.trim() == '')
         return 0;
-    return parseInt(numbers);
+
+    let [a,b] = numbers.split(',', 2)
+
+    console.log("a,b", a, b)
+    let m,n;
+    if(a == '' || isNaN(parseInt(a)) ) m = 0
+    else m = parseInt(a)
+    if(b == '' || isNaN(parseInt(b))) n = 0
+    else n = parseInt(b)
+
+    console.log("m,n", m, n);
+
+    return m + n;
 }
