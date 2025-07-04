8d6d8efd875e4dfe5664bd97841f2071e5d4ed30
Anish59312 <anish31442@gmail.com>
2025-07-05 00:12:12 +0530

    [step-9] added test for delimiter of any length - passed

--- src/lib/add.test.ts
+++ src/lib/add.test.ts
@@ -41,4 +41,8 @@ describe("string calculator", () => {
       expect(add("//[*][%]\n1*2%3")).toBe(6);
     });
 
+    test("multiple delimiters of any length", () => {
+      expect(add("//[**][%]\n1**2%3")).toBe(6);
+    });
+
 })
